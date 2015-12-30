ad5d1b604aaeec8b2efed06ff9ecf573e6d1d27d
trfabienne <tr.fabienne@gmail.com>
2015-12-30 15:05:58 +0100

    Create Traitement.java

---
+++ src/main/java/Class/Traitement.java
@@ -0,0 +1,52 @@
+package Class;
+
+import java.util.LinkedList;
+import java.util.List;
+
+
+/**
+ * Created by tatsu on 13/11/2015.
+ */
+public class Traitement {
+
+    public List<String> traitement(String p_message) {
+        return separeLesDonnees(p_message);
+    }
+
+    private List<String> separeLesDonnees(String p_message) {
+        List<String> donnees = new LinkedList<String>();
+        String chaineTemporaire ="";
+
+        for (int i=0; i<p_message.length(); i++) {
+            if (p_message.charAt(i) == ' ') {
+                donnees.add(chaineTemporaire);
+                chaineTemporaire = "";
+            } else {
+                chaineTemporaire += p_message.charAt(i);
+            }
+        }
+        donnees.add(chaineTemporaire);
+        return donnees;
+    }
+
+    public void traitementEnCours(int tempsMilli) throws InterruptedException {
+        System.out.println();
+        Thread.sleep(tempsMilli);
+        System.out.print("Class.Traitement en cours.");
+        Thread.sleep(tempsMilli);
+        System.out.print(".");
+        Thread.sleep(tempsMilli);
+        System.out.println(".\n");
+        Thread.sleep(tempsMilli);
+        System.out.println();
+
+    }
+
+    public void afficheDameuseDisponible(LesDameuses lesDameuses) {
+        System.out.println("Voici les dameuses qui ont été en mouvement : ");
+        for (Dameuse d : lesDameuses.getLesDameuses()){
+            System.out.println(d.getNom());
+
+        }
+    }
+}
